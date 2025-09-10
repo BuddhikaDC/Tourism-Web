bd4d7fe77bafb388133143664f818dfa3033090d
BuddhikaDC <buddhika1258@gmail.com>
2025-09-10 19:15:59 +0530

    Add space section placeholder in App component

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,11 @@ function App() {
     <>
       <NavBar />
       <Hero />
+      
+      {/* Space Section */}
+      <section className="py-20 bg-gray-100">
+      </section>
+
       <div className="py-16 bg-gradient-to-b from-gray-800 to-gray-900">
         <div className="container mx-auto px-4">
           <RideBookingForm />
